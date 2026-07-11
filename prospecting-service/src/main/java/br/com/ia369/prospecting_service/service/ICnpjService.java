5be832650354533432b3e54ffe2203945e0c356c
audalio-devops <audalio.devops@gmail.com>
2026-07-11 17:52:01 -0300

    Correcoes e Teste

--- prospecting-service/src/main/java/br/com/ia369/prospecting_service/service/ICnpjService.java
+++
@@ -1,8 +0,0 @@
-package br.com.ia369.prospecting_service.service;
-
-import br.com.ia369.prospecting_service.dto.CNPJResponse;
-import java.util.Optional;
-
-public interface ICnpjService {
-    Optional<CNPJResponse> findByCnpj(String cnpj);
-}
