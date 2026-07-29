6df1214719781f1b0ddece3679d8a69ef0054d35
audalio-devops <audalio.devops@gmail.com>
2026-07-29 19:41:24 -0300

    feat: add Docker support and Vite configuration for frontend application

--- frontend-app/vite.config.js
+++ frontend-app/vite.config.js
@@ -1,30 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const VA_TARGET = process.env.VITE_VA_API_URL || 'http://localhost:8080';
+const PS_TARGET = process.env.VITE_PS_API_URL || 'http://localhost:8081';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000, // O servidor de desenvolvimento do frontend rodará na porta 3000
+    port: 3000,
+    host: true, // Necessário para possibilitar o redirecionamento de portas do Docker Container para o Host
     proxy: {
       // Redireciona requisições para o virtual-assistant (porta 8080)
       '/api': {
-        target: 'http://localhost:8080',
+        target: VA_TARGET,
         changeOrigin: true,
       },
       '/chat': {
-        target: 'http://localhost:8080',
+        target: VA_TARGET,
         changeOrigin: true,
       },
       '/cnpj': {
-        target: 'http://localhost:8080',
+        target: VA_TARGET,
         changeOrigin: true,
       },
       // Redireciona requisições para o prospecting-service (porta 8081)
       '/prospecting': {
-        target: 'http://localhost:8081',
+        target: PS_TARGET,
         changeOrigin: true,
       }
     }
   }
 })
+
